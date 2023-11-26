a9778bb9d7d1dc08c6dccf2b6523962a6673da0c
nyanyacyan <nyanyacyan@gmail.com>
2023-11-27 05:30:51 +0900

    修正完了　 lambdaテストOK

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -4,22 +4,36 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import csv
+import logging
+
+# ローカルデバック時に使用
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
 
 
-def lambda_handler():
+def lambda_handler(event, context):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
     url = "https://www.petpochitto.com/"
 
-
     options = webdriver.ChromeOptions()
-
-    # headless-chromiumのパスを指定
-    options.binary_location = "/opt/headless/headless-chromium"
     options.add_argument("--headless")
-    options.add_argument('--single-process')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--hide-scrollbars")
+    options.add_argument("--single-process")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--window-size=880x996")
     options.add_argument("--no-sandbox")
+    options.add_argument("--homedir=/tmp")
+    options.binary_location = "/opt/headless/headless-chromium"
 
-    driver = webdriver.Chrome(
+    # ローカルデバック時に使用
+    # service = Service(ChromeDriverManager().install())
+    # browser = webdriver.Chrome(service=service)
+
+
+    browser = webdriver.Chrome(
         # chromedriverのパスを指定
         executable_path="/opt/headless/chromedriver",
         options=options
@@ -29,24 +43,29 @@ def lambda_handler():
     jan_details = {}
 
     # URLを開く
-    driver.get(url)
+    browser.get(url)
+
+    # URLのロード状況をログに記録
+    logger.info("Opened URL: " + url)
 
     # cdvを読み込む
     with open('aws_test.csv') as f:
         jans = csv.reader(f)
-        for jan in jans:
+        rows = list(jans)
+        print(rows)
 
+        for jan in jans:
             # 検索欄のidを取得
-            element = WebDriverWait(driver, 6).until(
-                EC.EC.element_to_be_clickable((By.ID, "keyword"))
+            element = WebDriverWait(browser, 60).until(
+                EC.element_to_be_clickable((By.ID, "keyword"))
             )
 
             # テキストボックスに検索ワードを入力
             element.send_keys(jan)
 
             # 検索ボタンを探す
-            # 見つけるまで最大6秒、動的待機
-            search_buttan = WebDriverWait(driver, 6).until(
+            # クリックができるようになるまで最大6秒、動的待機
+            search_buttan = WebDriverWait(browser, 6).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, ".__button.c-button"))
             )
 
@@ -56,7 +75,7 @@ def lambda_handler():
 
             # ddタグを見つける
             # 見つけるまで最大6秒、動的待機
-            all_ddtags = WebDriverWait(driver, 6).until(
+            all_ddtags = WebDriverWait(browser, 6).until(
                 EC.presence_of_all_elements_located((By.TAG_NAME, 'dd'))
             )
 
@@ -66,7 +85,7 @@ def lambda_handler():
 
             # h2タグを見つける
             # 見つけるまで最大6秒、動的待機
-            all_titles = WebDriverWait(driver, 6).until(
+            all_titles = WebDriverWait(browser, 6).until(
                 EC.presence_of_all_elements_located((By.TAG_NAME, 'h2'))
             )
 
@@ -77,14 +96,26 @@ def lambda_handler():
             # 辞書にまとめる
             jan_details[jan[0]] = (first_title, first_ddtag)
 
-    # output.csvに出力
-    with open('output.csv', 'w',newline='', encoding='utf-8') as file:
+    # CSVファイルの生成
+    with open('/tmp/output.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        for index, (key,(title, price)) in enumerate(jan_details.items(), start=1):
+        for index, (key, (title, price)) in enumerate(jan_details.items(), start=1):
             writer.writerow([index, key, title, price])
+
+    # CSVファイルの読み込み
+    with open('/tmp/output.csv', 'r', encoding='utf-8') as file:
+        csv_data = file.read()
     
     
     # WebDriverを閉じる
-    driver.quit()
+    browser.quit()
 
-lambda_handler()
+    # CSVデータをレスポンスとして返す
+    return {
+        'statusCode': 200,
+        'body': csv_data,
+        'headers': {
+            'Content-Type': 'text/csv',
+            'Content-Disposition': 'attachment; filename="output.csv"'
+        }
+    }
--- selenium_test.py
+++ selenium_test.py
@@ -3,49 +3,53 @@ from selenium import webdriver  # seleniumのバージョンを4.1にする
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager  # Chromeのバージョンをオートで確認してくれてインストールしてくれる
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import csv
 
 url = "https://www.petpochitto.com/"
 
 service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service)
+browser = webdriver.Chrome(service=service)
 
 
 def lambda_handler():
     # 空の辞書を設置
     jan_details = {}
     # URLを開く
-    driver.get(url)
+    browser.get(url)
 
     # cdvを読み込む
     with open('aws_test.csv') as f:
         jans = csv.reader(f)
+        rows = list(jans)
+        print(rows)
         for jan in jans:
 
             # 検索欄のidを取得
-            element = driver.find_element(By.ID, "keyword")
-            print(element)
+            element = WebDriverWait(browser, 6).until(
+                EC.element_to_be_clickable((By.ID, "keyword"))
+            )
 
             # テキストボックスに検索ワードを入力
             element.send_keys(jan)
 
             # 検索ボタンを探す
-            search_buttan = driver.find_element(By.CSS_SELECTOR,".__button.c-button")
+            search_buttan = browser.find_element(By.CSS_SELECTOR,".__button.c-button")
 
             # 検索ボタンをクリック
             search_buttan.click()
 
             # 現在のページにある価格を全てリスト化する。
-            all_ddtag_list = [dd_tag.text for dd_tag in driver.find_elements(By.TAG_NAME, 'dd')]
+            all_ddtag_list = [dd_tag.text for dd_tag in browser.find_elements(By.TAG_NAME, 'dd')]
             first_ddtag = all_ddtag_list[0]
 
             # 商品カタログ名
-            all_title_list = [title.text for title in driver.find_elements(By.TAG_NAME, 'h2')]
+            all_title_list = [title.text for title in browser.find_elements(By.TAG_NAME, 'h2')]
             first_title = all_title_list[0]
 
             # 辞書にまとめる
             jan_details[jan[0]] = (first_title, first_ddtag)
-            print(jan_details)
 
     # output.csvに出力
     with open('output.csv', 'w',newline='', encoding='utf-8') as file:
@@ -55,6 +59,6 @@ def lambda_handler():
     
     
     # WebDriverを閉じる
-    driver.quit()
+    browser.quit()
 
 lambda_handler()
